f4b6301311a8d01ddad48aa2d3c243aa74c80387
Navin Kumar <navinkumar0299@gmail.com>
2018-03-25 23:15:13 +0530

    Minor Changes in HomeScreen & Register

--- HomeScreen.c
+++ HomeScreen.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+//#include "Register.h"
+//#include "Login.h"
 
 int main()
 {
--- Register.c
+++ Register.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct member
 {
+	char UserName[30];
+	char Password[30];
+	char LibraryNumber[10];
 	char Name[30];
 	char Address[100];
 	char MobileNumber[11];
@@ -16,20 +20,64 @@ int main()
 	system("clear");
 	printf("\t\t\tRegister\n\n");
 	printf("Enter Following Details:\n");
+	printf("Username : ");
+	//fflush(stdin);
+	fgets(m.UserName, 30, stdin);
+	//scanf("%s", &m.UserName);
+	printf("Password : ");
+	//fflush(stdin);
+	fgets(m.Password, 30, stdin);
+	//scanf("%s", &m.Password);
+	printf("Library Number : ");
+	//fflush(stdin);	
+	fgets(m.LibraryNumber, 10, stdin);
+	//scanf("%s" , &m.LibraryNumber);
 	printf("Name : ");
-	scanf("%s", &m.Name);
+	//fflush(stdin);
+	fgets(m.Name, 30, stdin);
+	//scanf("%s", &m.Name);
+
 	printf("Address: ");
-	scanf("%s", &m.Address);
+	//fflush(stdin);
+	fgets(m.Address, 100, stdin);
+	//scanf("%s", &m.Address);
 	printf("Mobile Number: ");
-	scanf("%s", &m.MobileNumber);
+	//fflush(stdin);
+	fgets(m.MobileNumber, 11, stdin);
+	//scanf("%s", &m.MobileNumber);
 	printf("Email id: ");
-	scanf("%s", &m.EmailID);
+	//fflush(stdin);
+	fgets(m.EmailID, 30, stdin);
+	//scanf("%s", &m.EmailID);
 
 	printf("You have entered the following details:\n\n");
+	printf("Username : %s\n", m.UserName);
+	printf("Password : %s\n", m.Password);
+	printf("Library Number : %s\n", m.LibraryNumber);
 	printf("Name : %s\n", m.Name);
 	printf("Address : %s\n", m.Address);
 	printf("Mobile Number : %s\n", m.MobileNumber);
-	printf("EmailID : %s\n", m.EmailID);
+	printf("EmailID : %s\n xx", m.EmailID);
+
+	FILE * outfile;
+	outfile = fopen("Members.txt", "a");
+
+	if(outfile == NULL)
+	{
+		fprintf(stderr ,"ERROR opening file\n");
+		exit(1);
+	}
+	
+	fflush(stdin);
+
+	fwrite(&m, sizeof(struct member), 1, outfile);
+
+	if(fwrite != 0)
+		printf("Successfully Written\n");
+	else
+		printf("Error in Writing \n");
+
+	fclose(outfile);
 
 	return 0;
 	
